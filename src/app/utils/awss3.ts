250347b159ebc4c92b916cc155f13adb98d4d1b3
junayednoman <junayednoman05@gmail.com>
2026-01-02 11:43:06 +0600

    fix: aws config updated

--- src/app/utils/awss3.ts
+++ src/app/utils/awss3.ts
@@ -1,57 +1,57 @@
 import {
   DeleteObjectCommand,
-  ObjectCannedACL,
   PutObjectCommand,
-} from '@aws-sdk/client-s3';
-import { S3Client } from '@aws-sdk/client-s3'
-import { AppError } from '../classes/appError';
-import config from '../config';
-import { TFile } from '../../interface/file.interface';
-import multer, { memoryStorage } from 'multer';
+} from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+import { AppError } from "../classes/appError";
+import config from "../config";
+import { TFile } from "../../interface/file.interface";
+import multer, { memoryStorage } from "multer";
 
 export const s3Client = new S3Client({
-  endpoint: config.aws.endpoint,
+  // endpoint: config.aws.endpoint,
   region: `${config.aws.region}`,
   credentials: {
     accessKeyId: `${config.aws.accessKeyId}`,
     secretAccessKey: `${config.aws.secretAccessKey}`,
   },
-})
+});
 
 export const upload = multer({
   storage: memoryStorage(),
-})
+});
 
 //upload a single file
 export const uploadToS3 = async (file: TFile): Promise<string> => {
-  const fileName = `images/noman/${Date.now()}-${file.originalname}`;
+  const fileName = `yalla-coupon/${Date.now()}-${file.originalname}`;
 
   const command = new PutObjectCommand({
     Bucket: config.aws.bucket,
     Key: fileName,
     Body: file.buffer,
     ContentType: file.mimetype,
-    ACL: ObjectCannedACL.public_read, //access public read
   });
 
   try {
     const key = await s3Client.send(command);
     if (!key) {
-      throw new AppError(400, 'File Upload failed');
+      throw new AppError(400, "File Upload failed");
     }
-    const url = `${config?.aws?.s3BaseUrl}/${fileName}`;
-    if (!url) throw new AppError(400, 'File Upload failed');
+    const url = `${config?.aws?.s3BaseUrl}${fileName}`;
+    if (!url) throw new AppError(400, "File Upload failed");
 
     return url;
   } catch (error) {
     console.log(error);
-    throw new AppError(400, 'File Upload failed');
+    throw new AppError(400, "File Upload failed");
   }
 };
 
 // // delete file from s3 bucket
 export const deleteFromS3 = async (url: string) => {
-  const key = decodeURIComponent(url.split(".digitaloceanspaces.com/")[1]);
+  const key = decodeURIComponent(
+    url.split(config.aws.s3BaseUrl as string)[1] as string
+  );
 
   try {
     const command = new DeleteObjectCommand({
@@ -60,6 +60,6 @@ export const deleteFromS3 = async (url: string) => {
     });
     await s3Client.send(command);
   } catch (error) {
-    console.log('🚀 ~ deleteFromS3 ~ error:', error);
+    console.log("🚀 ~ deleteFromS3 ~ error:", error);
   }
 };
