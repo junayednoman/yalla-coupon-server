bfe65e6f0b34e83019b98aa05cdfc97e8ef2d60b
junayednoman <junayednoman05@gmail.com>
2026-01-26 13:19:55 +0600

    feat: category udpate api issue solved

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -65,9 +65,6 @@ const updateCategory = async (
     throw new AppError(400, "Invalid category ID!");
   }
 
-  const existingWithName = await Category.findOne({ name: payload.name });
-  if (existingWithName) throw new AppError(400, "Category already exists!");
-
   if (files?.image?.length) payload.image = await uploadToS3(files.image[0]);
   if (files?.arabicImage?.length)
     payload.arabicImage = await uploadToS3(files.image[0]);
